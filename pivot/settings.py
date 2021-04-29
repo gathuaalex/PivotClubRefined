7d5c300b5f8660d3cacd610df6474b4ae087825f
Ronnir-Leon76 <ronleon76@gmail.com>
2021-04-29 20:26:16 +0300

    Hide secret keys

--- pivot/settings.py
+++ pivot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import cloudinary, os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%$tl3s@@2-4$$+f4+)k4e(c$kix2hh+iz#7+3%#$-0b0t7r$wv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = ['afternoon-citadel-78534.herokuapp.com','127.0.0.1']
 
@@ -144,8 +145,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 cloudinary.config( 
   cloud_name = "ericouma", 
-  api_key = "272724916455615", 
-  api_secret = "a3d1T3zp37J0jJLUoPvXv6yHQH0" 
+  api_key = config('API_KEY'), 
+  api_secret = config('API_SECRET')
 )
 
 
